d3cc4c197f42eecd32d28d47acdedbd642fb79a7
Ben Micol <benmicol@gmail.com>
2020-08-27 17:10:04 -0400

    fixed methods and added css

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,13 +3,26 @@ require 'csv'
 
 class ApplicationController < ActionController::Base
 	def index
-		
-		@id = rand(0..77)
-		@raw_title = get_mov(@id)
-		@title = URI.escape(@raw_title)
-		@rating = get_rating_by_title(@title)
-		@image = get_image_by_title(@title)
+		@idRow = rand(0..226)
+		@id = get_mov(@idRow)		
+		@rating = get_info(@id, 'imdbRating')
+		@image = get_info(@id, 'Poster')
+		@title = get_info(@id, 'Title')
+		@year = get_info(@id, 'Year')
+		@cast = get_info(@id, 'Actors')
 	end
+	def get_info(id,item)
+		response = HTTP.get("http://www.omdbapi.com/?apikey=efdd29c4&i=#{id}").to_s
+		parsed_response = JSON.parse(response)
+		parsed_response[item]
+	end
+	def get_mov(random)
+		contents = CSV.parse(File.read("app/assets/CSV/data.csv"), headers: true)
+		id_arr = contents.by_col[0]
+		mov_id = id_arr[random]
+	end
+end
+=begin
 	def get_rating_by_title(title)
 		response = HTTP.get("http://www.omdbapi.com/?apikey=efdd29c4&t=#{title}").to_s
 		parsed_response = JSON.parse(response)
@@ -21,23 +34,19 @@ class ApplicationController < ActionController::Base
 		parsed_response["Poster"]
 	end
 	def get_rating_by_id(id)
-		response = HTTP.get("http://www.omdbapi.com/?apikey=efdd29c4&i=tt#{id}").to_s
+		response = HTTP.get("http://www.omdbapi.com/?apikey=efdd29c4&i=#{id}").to_s
 		parsed_response = JSON.parse(response)
 		parsed_response["imdbRating"]
 	end
 	def get_image_by_id(id)
-		response = HTTP.get("http://www.omdbapi.com/?apikey=efdd29c4&t=tt#{id}").to_s
+		response = HTTP.get("http://www.omdbapi.com/?apikey=efdd29c4&i=#{id}").to_s
 		parsed_response = JSON.parse(response)
 		parsed_response["Poster"]
 	end
 	def get_title_by_id(id)
-		response = HTTP.get("http://www.omdbapi.com/?apikey=efdd29c4&t=tt#{id}").to_s
+		response = HTTP.get("http://www.omdbapi.com/?apikey=efdd29c4&t=#{id}").to_s
 		parsed_response = JSON.parse(response)
 		parsed_response["Title"]
 	end
-	def get_mov(random)
-		contents = CSV.parse(File.read("app/assets/CSV/movies.csv"), headers: true)
-		title_arr = contents.by_col[0]
-		mov_title = title_arr[random]
-	end
-end
+	@title = URI.escape(@id)
+=end
