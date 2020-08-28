ef1919cf1cd10d2aab9a46971c5bba6813bf1d79
Ben Micol <benmicol@gmail.com>
2020-08-28 15:25:36 -0400

    fixed double load by removing turbolinks and refined random score

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,15 +1,41 @@
-require 'uri'
+#require 'uri'
 require 'csv'
 
 class ApplicationController < ActionController::Base
 	def index
 		@idRow = rand(0..226)
 		@id = get_mov(@idRow)		
-		@rating = get_info(@id, 'imdbRating')
 		@image = get_info(@id, 'Poster')
 		@title = get_info(@id, 'Title')
 		@year = get_info(@id, 'Year')
 		@cast = get_info(@id, 'Actors')
+		start = rand(1..3)
+		@ratingCheck = get_info(@id, 'imdbRating')
+		@rating = ''
+		@rating1 = ''
+		@rating2 = ''
+		ratingArr = 6.0.step(by: 0.1, to: 9.5).to_a
+		avail = ratingArr.select {|x| x < (@ratingCheck.to_f - 0.2) || x > (@ratingCheck.to_f + 0.2) }
+		avail2 = avail.select {|x| x > (@ratingCheck.to_f - 1.0) && x < (@ratingCheck.to_f + 1.0)}
+		arrRand = rand(0..avail2.length)
+		lower = @ratingCheck.to_f - 1
+		upper = @ratingCheck.to_f + 1
+		seed1 = avail2[rand(0..avail2.length-1)]
+		avail3 = avail2 - [seed1]
+		seed2 = avail3[rand(0..avail3.length-1)]
+		if start == 1
+			@rating = @ratingCheck
+			@rating1 = seed1
+			@rating2 = seed2
+		elsif start == 2
+			@rating1 = @ratingCheck
+			@rating = seed1
+			@rating2 = seed2
+		else
+			@rating2 = @ratingCheck
+			@rating = seed1
+			@rating1 = seed2
+		end
 	end
 	def get_info(id,item)
 		@api = ENV["API_KEY"]
