48a40c314207cd65e68fe90e46119182af975147
Ben Micol <benmicol@gmail.com>
2020-09-02 19:16:53 -0400

    added buttons

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,42 @@
 require 'csv'
 
 class ApplicationController < ActionController::Base
+	def test
+		
+	end
+	def play
+		@idRow = rand(0..226)
+		@id = get_mov(@idRow)		
+		@image = get_info(@id, 'Poster')
+		@title = get_info(@id, 'Title')
+		@year = get_info(@id, 'Year')
+		@cast = get_info(@id, 'Actors')
+		start = rand(1..3)
+		@ratingCheck = get_info(@id, 'imdbRating')
+		ratingArr = 6.0.step(by: 0.1, to: 9.5).to_a
+		avail = ratingArr.select {|x| x < (@ratingCheck.to_f - 0.2) || x > (@ratingCheck.to_f + 0.2) }
+		avail2 = avail.select {|x| x > (@ratingCheck.to_f - 1.0) && x < (@ratingCheck.to_f + 1.0)}
+		arrRand = rand(0..avail2.length)
+		seed1 = avail2[rand(0..avail2.length-1)]
+		avail3 = avail2 - [seed1]
+		seed2 = avail3[rand(0..avail3.length-1)]
+		if start == 1
+			@rating = @ratingCheck
+			@rating1 = seed1
+			@rating2 = seed2
+		elsif start == 2
+			@rating1 = @ratingCheck
+			@rating = seed1
+			@rating2 = seed2
+		else
+			@rating2 = @ratingCheck
+			@rating = seed1
+			@rating1 = seed2
+		end
+	end
+	def next
+		@idRow = rand(0..226)
+	end
 	def index
 		@idRow = rand(0..226)
 		@id = get_mov(@idRow)		
@@ -11,9 +47,6 @@ class ApplicationController < ActionController::Base
 		@cast = get_info(@id, 'Actors')
 		start = rand(1..3)
 		@ratingCheck = get_info(@id, 'imdbRating')
-		@rating = ''
-		@rating1 = ''
-		@rating2 = ''
 		ratingArr = 6.0.step(by: 0.1, to: 9.5).to_a
 		avail = ratingArr.select {|x| x < (@ratingCheck.to_f - 0.2) || x > (@ratingCheck.to_f + 0.2) }
 		avail2 = avail.select {|x| x > (@ratingCheck.to_f - 1.0) && x < (@ratingCheck.to_f + 1.0)}
@@ -46,6 +79,7 @@ class ApplicationController < ActionController::Base
 		id_arr = contents.by_col[0]
 		mov_id = id_arr[random]
 	end
+	
 end
 =begin
 	def get_rating_by_title(title)
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
 	root to: 'application#index'
+	#resources :application
+	#get '/play' => 'application#index'
+	post '/play' => 'application#index', as: :play
 end
